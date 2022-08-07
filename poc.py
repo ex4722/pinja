4353844d0bcaa1359f9bdf5d088a49a1482b992e
Eddie Xiao <ex47222@gmail.com>
2022-08-07 12:38:03 -0400

    Added POC.py

---
+++ poc.py
@@ -0,0 +1,36 @@
+from pwn import * 
+from os import system
+from binaryninja import BinaryViewType, RegisterValueType, MediumLevelILOperation
+from binaryninja.debugger import DebuggerController
+import binaryninja
+
+
+binary = "a.out"
+
+
+
+e = ELF(binary)
+e.asm(e.entrypoint, "h: jmp h;nop;nop")
+e.save("/tmp/" + binary)
+system("chmod +x /tmp/a.out")
+
+p = process("/tmp/" + binary)
+ti = BinaryViewType.get_view_of_file("./a.out")
+
+dbg = DebuggerController(ti)
+
+
+if dbg.attach(p.pid):
+    print("ATTACHED")
+else:
+    print("FAILED")
+
+
+bv = dbg.live_view 
+
+
+dbg.set_reg_value("rip", dbg.ip +2)
+dbg.add_breakpoint(bv.symbols["main"][0].address)
+
+# Breaks at main
+dbg.go()
