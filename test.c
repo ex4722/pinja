70691632df7613add341600d4a5596a835747257
Eddie Xiao <ex47222@gmail.com>
2022-08-06 22:14:46 -0400

    Inital Commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+    char a[10];
+    puts("HELLO WORLD");
+    gets(a);
+    for ( int i =0; i < 10; i ++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
