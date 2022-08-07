651470ae72512af2bc2505d4cd9256a21a3c89fb
Eddie Xiao <ex47222@gmail.com>
2022-08-07 15:14:25 -0400

    Comments + Unpatch patches

--- pinja.py
+++ pinja.py
@@ -3,29 +3,15 @@ from binaryninja.debugger import DebuggerController
 import binaryninja
 import pwn
 
-
-# class pinja(object):
-#     def __init__(self, filename):
-#         e = pwn.ELF(filename)
-#         e.asm(e.entrypoint, "h: jmp h;nop;nop")
-#         e.save("/tmp/" + filename)
-#         self.filename =  "/tmp/" + filename
-#         system(f"chmod +x {filename}")
-
-#         self.p = pwn.process(filename, aslr=False)
-#         ty = binaryninja.BinaryViewType.get_view_of_file(filename)
-#         self.dbg = DebuggerController(ty)
-#         if self.dbg.attach(self.p.pid):
-#             print("ATTACHED")
-#         else:
-#             print("FAILED")
-#         self.bv = self.dbg.live_view 
-#         self.dbg.set_reg_value("rip", self.dbg.ip +2)
-#         # self.dbg.go()
-
-
 class pinja(pwn.process):
     def __init__(self, filename):
+        """
+        exe is pwn ELF object 
+        filename is patched binary location 
+        bv is BN live view of process
+        dc is BN DebuggerController
+        """
+
         self.exe = pwn.ELF(filename)
         self.exe.asm(self.exe.entrypoint, "h: jmp h;nop;nop")
         self.exe.save("/tmp/" + filename)
@@ -43,7 +29,8 @@ class pinja(pwn.process):
             pwn.error(f"Error Attaching to process {self.pid}")
 
         self.bv = self.dc.live_view 
-        self.dc.set_reg_value("rip", self.dc.ip +2)
+        self.dc.set_reg_value("rip", self.bv.entry_point +4)
+        self.bv.write(self.bv.entry_point, pwn.asm("endbr64"))
 
     def close(self): 
         super().close()
