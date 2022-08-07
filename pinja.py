16493d4350a3844651ced649caec32778eb8f12a
Eddie Xiao <ex47222@gmail.com>
2022-08-07 14:32:54 -0400

    Fixes read issue

--- pinja.py
+++ pinja.py
@@ -26,14 +26,14 @@ import pwn
 
 class pinja(pwn.process):
     def __init__(self, filename):
-        e = pwn.ELF(filename)
-        e.asm(e.entrypoint, "h: jmp h;nop;nop")
-        e.save("/tmp/" + filename)
+        self.e = pwn.ELF(filename)
+        self.e.asm(self.e.entrypoint, "h: jmp h;nop;nop")
+        self.e.save("/tmp/" + filename)
 
         self.filename =  "/tmp/" + filename
         system(f"chmod +x {filename}")
 
-        super().__init__(filename)
+        super().__init__(self.filename)
 
         ty = binaryninja.BinaryViewType.get_view_of_file(filename)
         self.dc = DebuggerController(ty)
@@ -41,11 +41,11 @@ class pinja(pwn.process):
             print("ATTACHED")
         else:
             print("FAILED")
+
         self.bv = self.dc.live_view 
         self.dc.set_reg_value("rip", self.dc.ip +2)
 
     def close(self): 
-        print("CLOSE CALLED")
         super().close()
         self.bv.file.close()
         self.dc.destroy()
