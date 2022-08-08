9567d146709392b742374f6e38a966ca6923bf24
Eddie Xiao <ex47222@gmail.com>
2022-08-07 21:37:35 -0400

    Clones BN onto pwn

--- pinja.py
+++ pinja.py
@@ -1,16 +1,17 @@
+from unittest.mock import Mock
 from os import system
 from binaryninja.debugger import DebuggerController
 import binaryninja
 import pwn
 from pwnlib.util import packing
 
-class dc_process(pwn.process):
+class ninja_process(pwn.process):
     def __init__(self, filename):
         """
         exe is pwn ELF object 
         filename is patched binary location 
         bv is BN live view of process
-        dc is BN DebuggerController
+        dbg is BN DebuggerController
         """
 
         self.exe = pwn.ELF(filename)
@@ -23,25 +24,29 @@ class dc_process(pwn.process):
         super().__init__(self.filename)
 
         bv  = binaryninja.BinaryViewType.get_view_of_file(filename)
-        self.dc = DebuggerController(bv)
-        if self.dc.attach(self.pid):
+        self.dbg = DebuggerController(bv)
+        if self.dbg.attach(self.pid):
             pwn.info(f"Sucessfully Attached to process {self.pid}")
         else:
             pwn.error(f"Error Attaching to process {self.pid}")
 
-        self.bv = self.dc.live_view 
-        self.dc.set_reg_value("rip", self.bv.entry_point +4)
+        self.bv = self.dbg.live_view 
+        self.dbg.set_reg_value("rip", self.bv.entry_point +4)
         self.bv.write(self.bv.entry_point, pwn.asm("endbr64"))
 
-    def sendline(self, line=b''):
-        # Screw \n, If you want a newline put it in urself
-        line = packing._need_bytes(line)
-        self.send(line)
+        # Clones the BN debugger object, hopefully no name collisions
+        for attr in [method_name for method_name in dir(self.dbg) if not method_name.startswith("_") ]:
+            setattr(self,attr,  (getattr(self.dbg, attr)))
+
+    # def sendline(self, line=b''):
+    #     # Screw \n, If you want a newline put it in urself
+    #     line = packing._need_bytes(line)
+    #     self.send(line)
 
     def close(self): 
         super().close()
         self.bv.file.close()
-        self.dc.destroy()
+        self.dbg.destroy()
 
 
 # Mock everything
